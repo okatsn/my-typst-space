68d7d66ac522fdd46c171358010568ccd2d815f9
okatsn <okatsn@gmail.com>
2024-11-01 16:15:29 +0800

    Fix lefthook

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -1,11 +1,7 @@
 echo "Dockerfile modified. Building and pushing Docker image..."
 
-cd ./typst-official-build || exit 1
-
 docker build -t okatsn/my-typst-space:latest .
 
 docker push okatsn/my-typst-space:latest
 
 echo "Docker image built and pushed successfully."
-
-cd ..
