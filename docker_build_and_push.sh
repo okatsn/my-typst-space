6dfb7d62b5bba1f489f1e0295e53170507632240
okatsn <okatsn@gmail.com>
2025-04-16 09:17:04 +0800

    Comments on docker_build and push script.

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -2,6 +2,13 @@ echo "Dockerfile modified. Building and pushing Docker image..."
 
 docker build -t okatsn/my-typst-space:latest .
 
+
 docker push okatsn/my-typst-space:latest
+
+# Push the same image with the other tag:
+
+# docker tag okatsn/my-typst-space okatsn/my-typst-space:v2025a
+# docker push okatsn/my-typst-space:v2025a
+
 
 echo "Docker image built and pushed successfully."
